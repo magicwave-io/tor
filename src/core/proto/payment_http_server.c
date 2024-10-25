e47e5888b55de45f86bf92f86b63ca6c2bff3f43
a.gorulev <a.gorulev@kernelics.com>
2024-10-25 01:18:55 +0000

    magicwave.io

---
+++ src/core/proto/payment_http_server.c
@@ -0,0 +1,30 @@
+
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <ulfius.h>
+#include <orcania.h>
+#include "core/proto/payment_http_server.h"
+#define PORT 8537
+#define PREFIX "/test"
+#define PREFIXJSON "/testjson"
+#define PREFIXCOOKIE "/testcookie"
+
+
+#include "core/or/or.h"
+#include "core/or/origin_circuit_st.h"
+
+
+#include "core/or/circuitpayment.h"
+
+
+
+
+
+int start() {
+
+}
+
+
+
