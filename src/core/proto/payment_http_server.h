1153187c668a1dcd2542b0609b2028023f4f8072
a.gorulev <a.gorulev@kernelics.com>
2024-12-19 15:23:44 +0000

    magicwave.io

---
+++ src/core/proto/payment_http_server.h
@@ -0,0 +1,27 @@
+//
+// Created by edikk202 on 3/19/20.
+//
+
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <ulfius.h>
+
+
+
+
+int callback_get_test (const struct _u_request * request, struct _u_response * response, void * user_data);
+
+int callback_get_empty_response (const struct _u_request * request, struct _u_response * response, void * user_data);
+
+int callback_post_test (const struct _u_request * request, struct _u_response * response, void * user_data);
+
+int callback_all_test_foo (const struct _u_request * request, struct _u_response * response, void * user_data);
+
+int callback_get_cookietest (const struct _u_request * request, struct _u_response * response, void * user_data);
+
+int callback_default (const struct _u_request * request, struct _u_response * response, void * user_data);
+
+//public API
+int start ();
